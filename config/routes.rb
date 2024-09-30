00da75f926e13e96be5630cd7f812a5f16ab2c9f
keiske0408 <kieske0408@gmail.com>
2024-09-30 19:57:38 +0800

    creating post model,index,page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'about_us', to: 'welcome#about_us'
   get 'contact', to: 'welcome#contact'
+  get 'posts', to: 'posts#index'
+  post 'posts', to: 'posts#create'
+  get 'posts/new', to: 'posts#new'
 
 end
