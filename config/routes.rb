f025ed9ab9a2d7300c624e162b84299adfafab26
keiske0408 <kieske0408@gmail.com>
2024-10-07 16:15:36 +0800

    modify: routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'about_us', to: 'welcome#about_us'
   get 'contact', to: 'welcome#contact'
-  get 'posts', to: 'posts#index'
-  post 'posts', to: 'posts#create'
-  get 'posts/new', to: 'posts#new'
-  delete 'posts/:id', to: 'posts#destroy'
-  get 'posts/:id', to: 'posts#show'
-  get 'posts/:id/edit', to: 'posts#edit'
-  get 'posts/:id', to: 'posts#show', as: 'post'
-  patch 'posts/:id', to: 'posts#update'
-  put 'posts/:id', to: 'posts#update'
-
+  # get 'posts', to: 'posts#index'
+  # post 'posts', to: 'posts#create'
+  # get 'posts/new', to: 'posts#new'
+  # delete 'posts/:id', to: 'posts#destroy'
+  # get 'posts/:id', to: 'posts#show'
+  # get 'posts/:id/edit', to: 'posts#edit'
+  # get 'posts/:id', to: 'posts#show', as: 'post'
+  # patch 'posts/:id', to: 'posts#update'
+  # put 'posts/:id', to: 'posts#update'
+  resources :posts
 
 end
