b6988a7e82f97f1d31db7f9abc1ec262a6bb8b97
keiske0408 <kieske0408@gmail.com>
2024-10-07 15:19:03 +0800

    added: show,edit page, and destroy(in post act)

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,12 @@ Rails.application.routes.draw do
   get 'posts', to: 'posts#index'
   post 'posts', to: 'posts#create'
   get 'posts/new', to: 'posts#new'
+  delete 'posts/:id', to: 'posts#destroy'
+  get 'posts/:id', to: 'posts#show'
+  get 'posts/:id/edit', to: 'posts#edit'
+  get 'posts/:id', to: 'posts#show', as: 'post'
+  patch 'posts/:id', to: 'posts#update'
+  put 'posts/:id', to: 'posts#update'
+
 
 end
